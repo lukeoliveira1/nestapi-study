262c934410c727d953304e2c8a834ee625c73084
Lucas <lucasoliveirars22@gmail.com>
2024-09-29 15:43:57 -0300

    some validations

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,8 +1,18 @@
-export interface CreateUserDto {
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
   name: string;
+
+  @IsString()
   email: string;
-  cpf: string;
+
+  @IsString()
   phone: string;
+
+  @IsString()
+  cpf: string;
+
+  @IsString()
   password: string;
-  typeUser: number;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async findAll(): Promise<UserEntity[]> {
-    return await this.userService.findAll();
+  async findAll(): Promise<ReturnUserDto[]> {
+    return (await this.userService.findAll()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 
+  @UsePipes(ValidationPipe)
   @Post()
   async create(@Body() user: CreateUserDto): Promise<CreateUserDto> {
     return await this.userService.create(user);
